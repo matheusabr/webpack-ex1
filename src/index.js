290f841ccdd978defa78bb82aedf4ad603b5f05d
Matheus Abreu <matheusabreu.dev@gmail.com>
2019-02-23 22:22:38 +0000

    feat: using a babel plugin to transpile a spread operator

--- src/index.js
+++ src/index.js
@@ -9,3 +9,21 @@ logger.info("Using commonJS pattern!");
 // Using Class, Import and Export of ES6 (EcmaScript2015)
 const person = new Person("ES6 / EcmaScript2015");
 console.log(person.showName());
+
+const product = {
+  name: "JBL Charge 3",
+  price: 119.9,
+  discount: 9.9
+};
+
+console.log("product", product);
+
+// Using spread operator
+function cloneObj(object) {
+  return { ...object };
+}
+
+const newProduct = cloneObj(product);
+newProduct.name = "JBL Charge 4";
+
+console.log("newProduct", newProduct);
